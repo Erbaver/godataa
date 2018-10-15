7cf7183f4e69b834dd8267d698ec1e8a1091bcd2
Vincent Nwonah <vnwonah@outlook.com>
2018-10-15 12:38:11 +0100

    added godata website

---
+++ GoData.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoData.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
